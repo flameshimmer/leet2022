9afa0ed67718d904c03fec330387fad318483480
Ning Rogers <nm1072@gmail.com>
2022-05-31 12:26:36 -0700

    v4

---
+++ Solution2022.h
@@ -0,0 +1,6 @@
+namespace Solution2022
+{
+	namespace Test { void Main(); }
+
+};
+#pragma once
---
+++ Test.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+
+namespace Solution2022
+{
+	namespace Test {
+
+		void Main() {
+			print(string("Hello World"));
+		}
+	}
+
+}
+//end
